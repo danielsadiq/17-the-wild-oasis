05c7389cff32f0dd4dfc68ab4a053cd433d2d047
Daniel Sadiq <danielsadiq93@gmail.com>
2024-11-29 22:33:51 +0100

    TODAYY

--- src/services/apiSettings.js
+++ src/services/apiSettings.js
@@ -11,6 +11,7 @@ export async function getSettings() {
 }
 
 // We expect a newSetting object that looks like {setting: newValue}
+// TODAY
 export async function updateSetting(newSetting) {
   const { data, error } = await supabase
     .from("settings")

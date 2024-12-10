fe622b3ebdc5aa3aa485297b6b53ce4fbbb34dc3
Daniel Sadiq <danielsadiq93@gmail.com>
2024-12-10 22:26:44 +0100

    today

--- src/features/settings/useUpdateSetting.js
+++ src/features/settings/useUpdateSetting.js
@@ -14,6 +14,7 @@ export function useUpdateSetting() {
         queryKey: ["settings"],
       });
       //   reset();
+      // today
     },
     onError: (err) => toast.error(err.message),
   });
